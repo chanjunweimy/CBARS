9d6a2f19978572ed08a9785d0583e1e30549aab9
Chan Jun Wei <chanjunweimy@gmail.com>
2015-10-07 10:28:41 +0800

    Added Mahalanobis distance, but not sure how to fully implement it
    Signed-off-by: Chan Jun Wei <chanjunweimy@gmail.com>

---
+++ src/Distance/Mahalanobis.java
@@ -0,0 +1,50 @@
+package Distance;
+
+import org.apache.commons.math3.linear.LUDecomposition;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.stat.correlation.Covariance;
+
+/**
+ * Don't know how to get the covariance matrix
+ * 
+ * @author Jun
+ *
+ */
+public class Mahalanobis {
+
+	
+	public double getDistance(double[] query1, double[] query2, double[][] MFCC){
+		if (query1.length != query2.length){
+            System.err.println("Mahalanobis: The dimension of the two vectors does not match!");
+
+            System.exit(1);
+        }
+        double distance = 0;
+        
+        /*estimation of covariance matrix*/
+        double[][] dif = new double[1][query1.length];
+        for (int i = 0; i < query1.length; i++) {
+	        dif[0][i] = query1[i] - query2[i];
+        }
+        
+
+		RealMatrix difMatrix = MatrixUtils.createRealMatrix(MFCC);
+		RealMatrix cov = new Covariance(difMatrix).getCovarianceMatrix();
+		RealMatrix invCov = new LUDecomposition(cov).getSolver().getInverse();
+    	
+		
+		
+		// Left-hand side of the equation: vector * invcov^-1
+        double [] left = invCov.preMultiply(dif[0]);
+		
+        // Compute the dot product of both vectors
+        for (int i = 0; i < query1.length; i++) {
+        	distance += left[i] * dif[0][i];
+        }
+        
+		distance = Math.sqrt(distance);
+        return distance;
+        
+    }
+}
